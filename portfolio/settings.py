2c1aef39ea750d7c9a415b8f173571ad47d63215
Wendrew Oliveira <developer.wendrew@gmail.com>
2026-06-01 09:24:15 -0300

    static files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'portfolio' / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
